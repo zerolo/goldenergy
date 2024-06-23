5accc26147e82aaf9299480585d718df0f74462d
zerolo <ze.p.rolo@gmail.com>
2024-06-23 16:56:37 +0100

    added description to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_namespace_packages
 setup(
     name="goldenergy",
     version="0.0.1",
+    description="Library to interact with Goldenergy API. Intended to be used for Home Assistant integration",
     python_requires='>=3',
     package_dir={'': 'src'},
     packages=find_namespace_packages(where='src'),
