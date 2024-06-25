31786b903eba528be7defb89dd266e094a2f2d19
zerolo <ze.p.rolo@gmail.com>
2024-06-25 20:12:07 +0100

    update to version 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name="goldenergy",
-    version="0.0.1",
+    version="0.0.2",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     python_requires='>=3',
